99d4ea5a8c9aabbacaa34f3abf2eb917c9f4708f
Edward Bae <edwardbae@yahoo.com>
2016-06-18 02:52:22 +0200

    interface simplify

--- client/JS/closeMenuOnClick.js
+++ client/JS/closeMenuOnClick.js
@@ -1,6 +1,24 @@
-//closes bootstrap collapiseble menu
+// closes bootstrap collapiseble menu on clicking outside menu
 $(document).click(function(e) {
     if (!$(e.target).is('a')) {
         $('.collapse').collapse('hide');
     }
 });
+//closes bootstrap collapsible menu on clicking a link
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
+//add active class to highlight
+// $(function(){
+//     var tempVar = Session.get('pageTitle')
+//     if (tempVar==='나의 기도') {
+//         $("#thisIsmyPrayer").addClass("active");
+//     }
+// });
+
+$(function () {
+    $("#navbar a:contains('aaa')").parent().addClass('active');
+    $("#navbar a:contains('친구들의 기도들')").parent().addClass('active');
+ });
